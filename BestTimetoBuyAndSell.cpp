c63607f40d9c861bf70296fe7e1e799ea4351b7f
Sanjuksha Nirgude <snirgude@symbotic.com>
2022-01-26 11:29:30 -0800

    Problem 121: Best Time to buy and sell

---
+++ BestTimetoBuyAndSell.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+
+class Solution
+{
+public:
+    Solution()
+    {
+        l = 0;
+        r = 1;
+        maxP = 0;
+    }
+
+    int maxProfit(std::vector<int>& prices)
+    {
+        while( r < prices.size())
+        {
+            if(prices[l] < prices[r])
+            {
+                int profit;
+                profit = prices[r] - prices[l];
+                maxP = std::max(profit, maxP);
+            }
+            else
+            {
+                l = r;
+            }
+            r +=1;
+        }
+        return maxP;
+    }
+
+private:
+    int l;  ///< l = buy, initializing to 0
+    int r;  ///< r = sell, initializing to 1
+    int maxP; ///<Maximum profit
+};
+
+int main()
+{
+    std::cout<<"LeetCode Problem 121: Best Time to Buy and Sell"<<std::endl;
+    std::vector<int> prices;
+    prices = {7, 1, 5, 3, 6, 4};
+
+    Solution solution;
+    int result;
+    result = solution.maxProfit(prices);
+
+    std::cout<<"Max Profit: "<<result<<std::endl;
+}
